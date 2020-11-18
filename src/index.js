ce5e369bc7b1f957f351bc58861eb75b50f838bc
Andrew Fedina <veselchak@gmail.com>
2020-11-18 23:37:40 +0200

    Add scss loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style.scss";
 
 ReactDOM.render(<div>Hello!!!</div>, document.querySelector("#root"));
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -5,24 +5,26 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: ["/node_modules/"],
-				use: [
-					{
-						loader: "babel-loader",
-						options: {
-							presets: [
-								"@babel/preset-react",
-								[
-									"@babel/env",
-									{
-										targets: {
-											browsers: ["last 7 versions"],
-										},
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							"@babel/preset-react",
+							[
+								"@babel/env",
+								{
+									targets: {
+										browsers: ["last 7 versions"],
 									},
-								],
+								},
 							],
-						},
+						],
 					},
-				],
+				},
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 		],
 	},
