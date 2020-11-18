5d793ca6cecb1c32a27e39beb3ad8cb391522209
Andrew Veselchak <veselchak@gmail.com>
2020-11-18 20:17:24 +0200

    add config prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,41 +1,47 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-const merge = require("webpack-merge");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const { merge } = require("webpack-merge");
 const CompressionPlugin = require("compression-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const baseConfig = require("./webpack.config.base");
+const WebpackDevServer = require("webpack-dev-server");
 
 module.exports = merge(baseConfig, {
 	mode: "production",
 	entry: "./src/index.js",
 	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "public"),
+		filename: "bundle.[chunkhash].js",
+		path: path.resolve(__dirname, "dist"),
 		publicPath: "/",
 	},
 
 	plugins: [
+		new CompressionPlugin(),
 		new HtmlWebpackPlugin({
 			template: "./index.html",
 		}),
+		new CleanWebpackPlugin(),
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: JSON.stringify("production"),
+			},
+		}),
 	],
 
-	devtool: "inline-source-map",
 	optimization: {
+		minimize: true,
 		minimizer: [
-			new UglifyJsPlugin({
-				cache: true,
+			new TerserPlugin({
 				parallel: true,
-				uglifyOptions: {
-					compress: false,
-					ecma: 6,
+				terserOptions: {
 					mangle: true,
 				},
-				sourceMap: false,
 			}),
 		],
 	},
+
+	devtool: "inline-source-map",
 });
