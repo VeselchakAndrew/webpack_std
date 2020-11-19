3e7f648e7dc1529b5d7d39dddb34416335efe100
Andrew Fedina <veselchak@gmail.com>
2020-11-19 22:21:55 +0200

    Add redux

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const About = () => {
+import { setHello } from "../../../store/action";
+
+const About = ({ hello, setHello }) => {
 	return (
 		<>
 			<div>Hello from About</div>
 			<Link to="/">Home</Link>
+			<br />
+			{hello}
+			<br />
+			<button type="button" onClick={() => setHello("HELLO")}>
+				Hello
+			</button>
 		</>
 	);
 };
 
-export default About;
+const mapStateToProps = (state) => ({
+	hello: state.hello,
+});
+
+const mapDispatchToProps = { setHello };
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom";
 import "./style.scss";
 import { BrowserRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
+import { Provider } from "react-redux";
 
 import Routes from "./components/Routes/Routes";
+import { store } from "./store";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<div>
-			Hello!!!
-			{renderRoutes(Routes)}
-		</div>
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				Hello!!!
+				{renderRoutes(Routes)}
+			</div>
+		</BrowserRouter>
+	</Provider>,
 	document.querySelector("#root")
 );
